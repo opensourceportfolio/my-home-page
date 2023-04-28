5bd442cd7622409579d72b0c65152717c7f8df45
Leon Tager <leon.tager@airbnb.com>
2023-04-28 09:58:26 -0700

    making photo smaller

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -1,7 +1,7 @@
 export function ProfilePhoto() {
   return (
     <div
-      className='bg-cover mx-auto mb-8 lg:mb-32 rounded-full w-72 h-72'
+      className='bg-cover mx-auto mb-8 lg:mb-32 rounded-full w-48 h-48 md:w-72 md:h-72'
       style={{
         backgroundImage: 'url(self.jpg)',
         backgroundPosition: '100% 25%',
