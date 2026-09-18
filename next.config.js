124c3b5d1724be42be5b1b52fe9dc9efe06891bc
Leon Tager <leon.tager@airbnb.com>
2026-09-18 10:04:39 -0700

    update resume links and improve content clarity in resume section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{ source: '/resume', destination: '/resume.html' }];
+  },
+  async redirects() {
+    return [{ source: '/resume.html', destination: '/resume', permanent: true }];
+  },
 }
 
 module.exports = nextConfig
